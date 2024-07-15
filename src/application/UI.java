85026cf22aec16c386cc5243420d2406ee8c9eb7
Gabriel Pilati <gabrielms1504@gmail.com>
2024-07-15 20:46:23 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -43,6 +44,13 @@ public class UI {
 			throw new InputMismatchException("Invalid board position, values are from a1 to h8.");
 		}
 	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
 
@@ -58,7 +66,7 @@ public class UI {
 
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 
 		for (int i = 0; i < pieces.length; i++) {
@@ -75,11 +83,11 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
-		
-		if(background) {
+
+		if (background) {
 			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
-		
+
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
 		} else {
